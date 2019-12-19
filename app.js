ea27f759ca2583525409c3001e12eb19ed62fb74
Nikhil Buduma <nikhil@remedymedical.com>
2019-12-18 22:40:11 -0800

    ready to push

--- app.js
+++ app.js
@@ -1,42 +1,67 @@
 var faceapi = require('face-api.js');
 
+let inputSize = 512
+let scoreThreshold = 0.5
+
 
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 // Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger");
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
+const cameraOverlay = document.querySelector("#camera--overlay");
+
+const options = new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
+
+
+async function onPlay() {
+  const result = await faceapi.detectSingleFace(cameraView, options).withFaceLandmarks()
+  if (result) {
+    const dims = faceapi.matchDimensions(cameraOverlay, cameraView, true)
+    const resizedResult = faceapi.resizeResults(result, dims)
+    // faceapi.draw.drawDetections(cameraOverlay, resizedResult)
+    faceapi.draw.drawFaceLandmarks(cameraOverlay, resizedResult);
+    setTimeout(() => onPlay());
+  }
+
+  setTimeout(() => onPlay())
+}
+
+cameraView.onplay = onPlay
 
 // Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.error("Oops. Something is broken.", error);
-        });
+async function cameraStart() {
+
+  await faceapi.loadFaceLandmarkModel("models");
+  await faceapi.nets.tinyFaceDetector.loadFromUri('models')
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+      console.error("Oops. Something is broken.", error);
+    });
 }
+
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-    // track.stop();
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
+  // track.stop();
 };
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
 console.log(faceapi.nets);
 console.log("hello");
-
-faceapi.loadFaceLandmarkModel("models");
--- bundle.js
+++ bundle.js
@@ -1,46 +1,71 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var faceapi = require('face-api.js');
 
+let inputSize = 512
+let scoreThreshold = 0.5
+
 
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 // Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger");
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
+const cameraOverlay = document.querySelector("#camera--overlay");
+
+const options = new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
+
+
+async function onPlay() {
+  const result = await faceapi.detectSingleFace(cameraView, options).withFaceLandmarks()
+  if (result) {
+    const dims = faceapi.matchDimensions(cameraOverlay, cameraView, true)
+    const resizedResult = faceapi.resizeResults(result, dims)
+    // faceapi.draw.drawDetections(cameraOverlay, resizedResult)
+    faceapi.draw.drawFaceLandmarks(cameraOverlay, resizedResult);
+    setTimeout(() => onPlay());
+  }
+
+  setTimeout(() => onPlay())
+}
+
+cameraView.onplay = onPlay
 
 // Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.error("Oops. Something is broken.", error);
-        });
+async function cameraStart() {
+
+  await faceapi.loadFaceLandmarkModel("models");
+  await faceapi.nets.tinyFaceDetector.loadFromUri('models')
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+      console.error("Oops. Something is broken.", error);
+    });
 }
+
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-    // track.stop();
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
+  // track.stop();
 };
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
 console.log(faceapi.nets);
 console.log("hello");
-
-faceapi.loadFaceLandmarkModel("models");
 
 },{"face-api.js":320}],2:[function(require,module,exports){
 "use strict";
